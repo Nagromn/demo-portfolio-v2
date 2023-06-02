58037065a472474d8b46110175903d32228ba0b1
nagromn <neverlanddu02@hotmail.fr>
2023-06-02 19:29:47 +0200

    new AdminController, Category, Project Models, nouvelles pages et dashboard admin + correction de code et optimisation des routes

--- public/index.php
+++ public/index.php
@@ -6,10 +6,14 @@ use Routes\RouteConfig;
 require 'vendor/autoload.php';
 
 $router = new Router();
-$routes = RouteConfig::getRoutes();
 
-foreach ($routes as $route) {
-    $router->addRoute($route);
+try {
+    $routes = RouteConfig::getRoutes();
+    foreach ($routes as $route) {
+        $router->addRoute($route);
+    }
+} catch (ReflectionException $e) {
+    // Gérer l'exception ici
 }
 
 $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
--- routes/RouteConfig.php
+++ routes/RouteConfig.php
@@ -3,16 +3,45 @@
 namespace Routes;
 
 use App\Controller\HomeController;
+use App\Controller\AdminController;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionMethod;
 
 class RouteConfig
 {
+
+    /**
+     * @throws ReflectionException
+     */
     public static function getRoutes(): array
     {
         $routes = [];
 
-        // Ajouter les routes au tableau $routes
-        $routes[] = new Route('GET', '/', HomeController::class, 'index');
-        // ...
+        $controllers = [
+            HomeController::class => [
+                'home' => '/',
+                'professional' => '/professional',
+                'project' => '/project',
+                'skill' => '/skill',
+            ],
+            AdminController::class => [
+                'dashboard' => '/admin-dashboard',
+            ],
+        ];
+
+        foreach ($controllers as $controller => $routeMappings) {
+            $reflection = new ReflectionClass($controller);
+            $methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
+
+            foreach ($methods as $method) {
+                if ($method->class === $controller && $method->name !== '__construct') {
+                    $action = $method->name;
+                    $routePath = $routeMappings[$action] ?? '/notfound';
+                    $routes[] = new Route('GET', $routePath, $controller, $action);
+                }
+            }
+        }
 
         return $routes;
     }
